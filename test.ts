08fe89ac58ea130531d30dcd6241ca7e32a69e55
Cyryl Sochacki <cyrylsochacki@gmail.com>
2019-02-07 08:41:55 +0100

    Update test.ts

--- test.ts
+++ test.ts
@@ -18,6 +18,11 @@ Borsuk.ServoStop(Servo.S0)
 //Servo Turn Angle
 Borsuk.ServoTurnAngle(Servo.S1, 180)
 
-//
+//Servo Forward
 Borsuk.ServoMotorForward(Servo.S1, 180)
+
+//Servo Backward
+Borsuk.ServoMotorBackward(Servo.S1, 0)
+
+//Servo Motor Stop
 Borsuk.ServoMotorBackward(Servo.S1, 0)
